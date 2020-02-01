b83dba08621ebf9146f80f9d9c20b427e36a22da
Eden Reich <eden.reich@gmail.com>
2020-02-01 21:49:28 +0100

    remove default node pool on creation, also just for testing purposes create only one node in one availability zone

--- gke.tf
+++ gke.tf
@@ -1,8 +1,9 @@
 
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
-  location = var.region
+  location = var.zone
 
+  remove_default_node_pool = true
   initial_node_count = var.initial_node_count
 
   master_auth {
@@ -31,7 +32,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
