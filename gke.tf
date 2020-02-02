3de92f5ead31222f3552bd12406d500de89350a4
Eden Reich <eden.reich@gmail.com>
2020-02-02 18:42:21 +0100

    remove this extra space

--- gke.tf
+++ gke.tf
@@ -30,7 +30,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "n1-standard-1"
-
     disk_size_gb = 10
 
     metadata = {
